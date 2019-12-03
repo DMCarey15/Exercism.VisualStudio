666d2942664f0da7323d4a40c4c5a4691dbc5130
Deborah Carey <56282627+DMCarey15@users.noreply.github.com>
2019-12-03 08:07:25 -0500

    Add files via upload
    I am new to exercism and just starting to learn GitHub and C#

---
+++ csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercism11_25_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFunction();
+        }
+
+        static void HelloFunction ()
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+}
